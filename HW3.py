0b42cc50aaeaef76c1e26e17975b231e56d8b343
Tucker Hooper <th126148@student.wpsok.org>
2026-08-28 13:37:51 -0500

    HW3

---
+++ HW3.py
@@ -0,0 +1,29 @@
+#Name: Tucker Hooper
+#Class: 6th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+a=6
+b="hello"
+c=True
+#3. Print all three variables on the same print function (at the same time).
+print(a,b,c)
+#4. Create a variable that asks the user to input an integer.
+d=int(input("Enter an interger"))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+e=(a+d)
+print(e)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(e/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+c=False
+#8. Print the value of the boolean variable.
+print(c)
+#9. Create a variable with a number that contains decimals.
+f=2.7
+#10. Round the number from #9 up or down using the round function.
+g=round(f)
+print(g)
