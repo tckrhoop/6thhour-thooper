ea82fea8288af892064f8dd14441cdd3ad3fdc9f
Tucker Hooper <th126148@student.wpsok.org>
2026-09-09 13:56:41 -0500

    HW5

---
+++ HW6.py
@@ -0,0 +1,22 @@
+#Name: Tucker Hooper
+#Class: 6th Hour
+#Assignment: HW6
+
+
+#1. Create a list with 9 different numbers inside.
+
+#2. Sort the list from highest to lowest.
+
+#3. Create an empty list.
+
+#4. Remove the median number from the first list and add it to the second list.
+
+#5. Remove the first number from the first list and add it to the second list.
+
+#6. Print both lists.
+
+#7. Add the two numbers in the second list together and print the result.
+
+#8. Move the number back to the first list (like you did in #4 and #5 but reversed).
+
+#9. Sort the first list from lowest to highest and print it.
