2df6064565e5c1a5d1d8a864221e8334681503d7
Tucker Hooper <th126148@student.wpsok.org>
2026-09-10 13:40:21 -0500

    HW6

--- HW6.py
+++ HW6.py
@@ -4,19 +4,38 @@
 
 
 #1. Create a list with 9 different numbers inside.
+numbers=[1, 7, 3, 5, 9, 13, 15, 11, 17]
 
+print(numbers)
 #2. Sort the list from highest to lowest.
+numbers.sort(reverse=True)
 
+print(numbers)
 #3. Create an empty list.
+mt=[]
 
+print(mt)
 #4. Remove the median number from the first list and add it to the second list.
-
+num_sum=sum(numbers)
+print(num_sum)
+med=num_sum/9
+print(med)
+mt.insert(0,med)
+print(mt)
 #5. Remove the first number from the first list and add it to the second list.
-
+num_pop=numbers.pop(0)
+print(numbers)
+mt.insert(1,num_pop)
+print(mt)
 #6. Print both lists.
-
+print(numbers)
+print(mt)
 #7. Add the two numbers in the second list together and print the result.
-
-#8. Move the number back to the first list (like you did in #4 and #5 but reversed).
-
+mt_sum=(mt[0]+mt[1])
+print(mt_sum)
+#8. Add the sum from #7 to the first list.
+numbers.append(mt_sum)
+print(numbers)
 #9. Sort the first list from lowest to highest and print it.
+numbers.sort()
+print(numbers)
