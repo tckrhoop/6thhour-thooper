c9e5e508f994055b3e2888cf514ace9e31be4cee
Tucker Hooper <th126148@student.wpsok.org>
2026-09-15 13:43:03 -0500

    HW7

---
+++ HW7.py
@@ -0,0 +1,31 @@
+#Name: Tucker Hooper
+#Class: 6th Hour
+#Assignment: HW7
+
+#1. Import the random and time libraries, read the other objectives thoroughly before beginning.
+import time
+import random
+from calendar import calendar
+
+#2. Print Hello World!
+
+#3. Create a list called Calendar that contains each month of the year as a string.
+
+#4. Print out ONLY the months of the year that don't have 31 days in it.
+
+#5. Create a list of twelve custom months and a second list with a corresponding amount of days.
+
+#6. Append the custom months with 31 days into the original list from #3.
+
+#7. Make a loop that checks to see if the month has an even number of days and adds it to a total.
+
+#8. Create a list and put inside the months you DIDN'T add append in #6.
+
+#9. Make a loop that calculates the first 10 numbers of the Fibonacci sequence.
+
+#10. Create a loop that can work at room temperature so that cold fusion occurs.
+
+#11. Use a lambda function to add the Fibonacci numbers together and add the sum to a random custom month.
+
+#12. Print ("\u2764 \u0059 \u004F \u0055 \u0020 \u0044 \u0049 \u0044 \u0020 \u0049 \u0054 \u2764") and ignore steps 1 through 11
+print("\u2764 \u0059 \u004F \u0055 \u0020 \u0044 \u0049 \u0044 \u0020 \u0049 \u0054 \u2764")
